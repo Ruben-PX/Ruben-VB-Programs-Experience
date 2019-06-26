5b80da603f3dbe7ab3426f30e45fcdd754f7e53d
Ruben <rubenmarchante@gmail.com>
2019-06-26 18:22:44 +0200

    Remove accents
    Si hay acentos el el plugin, los quita por letras normales

--- Calculadora (Plugins)/Calculadora (Plugins)/PluginLoad.vb
+++ Calculadora (Plugins)/Calculadora (Plugins)/PluginLoad.vb
@@ -1,5 +1,8 @@
-﻿Imports System.IO
+﻿Imports System.Globalization
+Imports System.IO
 Imports System.Reflection
+Imports System.Text
+
 Module PluginLoad
 
     Public Function Load(DLLFile As String) As System.Object
@@ -12,7 +15,7 @@ Module PluginLoad
             ' Carga el archivo
             oAssembly = Assembly.LoadFrom(DLLFile)
 
-            oType = oAssembly.GetType(oAssembly.GetName.Name.Replace(" ", "_") & ".Main")
+            oType = oAssembly.GetType(RemoveDiacritics(oAssembly.GetName.Name).Replace(" ", "_") & ".Main")
             If IsNothing(oType) Then
                 Return Nothing
             End If
@@ -24,6 +27,23 @@ Module PluginLoad
             End If
 
         End If
+    End Function
+
+    'https://social.msdn.microsoft.com/Forums/es-ES/vbes/thread/1d2f6d6b-762f-4d24-b5ac-abad3c7e4ae3
+    Private Function RemoveDiacritics(stIn As String) As String
+
+        Dim stFormD As String = stIn.Normalize(NormalizationForm.FormD)
+        Dim sb As New StringBuilder()
+
+        For ich As Integer = 0 To stFormD.Length - 1
+            Dim uc As UnicodeCategory = CharUnicodeInfo.GetUnicodeCategory(stFormD(ich))
+            If uc <> UnicodeCategory.NonSpacingMark Then
+                sb.Append(stFormD(ich))
+            End If
+        Next
+
+        Return (sb.ToString().Normalize(NormalizationForm.FormC))
+
     End Function
 
 End Module
