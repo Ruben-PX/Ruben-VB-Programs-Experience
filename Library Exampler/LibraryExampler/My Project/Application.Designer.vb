044f908816f13e40adfda6ef6d48f1902d26bc2d
Ruben <rubenmarchante@gmail.com>
2019-06-29 16:44:02 +0000

    Library Example - Update
    Library Exampler:
    - [ Solucionado ] Se estaba haciendo una referencia a un formulario que no existe
    - Los plugins se compilan en la carpeta de plugins

--- Library Exampler/LibraryExampler/My Project/Application.Designer.vb
+++ Library Exampler/LibraryExampler/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Library_Exampler.Form1
+            Me.MainForm = Global.Library_Exampler.Main
         End Sub
     End Class
 End Namespace
